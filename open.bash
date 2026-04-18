c0f425516d4adb85f905271a31af5156428160ac
Kira <kira.eva@protonmail.com>
2026-04-18 17:02:35 +0100

    fix bash scripts

---
+++ open.bash
@@ -0,0 +1,8 @@
+STEAMPATH=/c/Games/Steam/steamapps/common/
+GAMEPATH=Kingdoms\ and\ Castles/
+
+cd $STEAMPATH || exit
+cd "$GAMEPATH" || exit
+cd "KingdomsAndCastles_Data/mods/KiraMod" || exit
+
+start .
